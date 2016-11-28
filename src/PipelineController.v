c47f75c187d7de184f4ea45f5b599e0f0f943f16
William Greenleaf <Orangelink0@gmail.com>
2016-11-27 19:17:26 -0500

    Added skeleton controller

---
+++ src/PipelineController.v
@@ -0,0 +1,9 @@
+//Pipeline Controller skeleton
+//TODOS:
+//add logic, add outputs needed that were added to Project2.v for the pipeline processor
+module PipelineController(EX_op, EX_func, MEM_op, MEM_func, WB_op, WB_func, 
+								  allowBr, brBaseMux, rs1Mux, rs2Mux, alu2Mux, 
+								  aluOp, cmpOp, wrReg, wrMem, dstRegMux);
+
+								  
+endmodule								  
