5b95020142b4ec17ba5e3ef3063353859ab40a9d
William Greenleaf <Orangelink0@gmail.com>
2016-12-02 12:15:56 -0500

    changing exreg back to more accurate form

--- src/EXreg.v
+++ src/EXreg.v
@@ -1,22 +1,22 @@
-module EXreg(wrt_en, reset, clk, func, op, regData2, intermediateResult, rs2, rd, ME_mux_sel, wrReg, wrMem,
-				 EX_func, EX_op, EX_regData2, EX_intermediateResult, EX_rs2, EX_rd, EX_ME_mux_sel, EX_wrReg, EX_wrMem); 
+module EXreg(wrt_en, reset, clk, EX_func, EX_op, regData2, intermediateResult, rs2, rd, ME_mux_sel, wrReg, wrMem,
+				 ME_func, ME_op, EX_regData2, EX_intermediateResult, EX_rs2, EX_rd, EX_ME_mux_sel, EX_wrReg, EX_wrMem); 
 
 	parameter DBITS = 32; 
 	parameter REG_INDEX_BIT_WIDTH = 4; 
 	
 	input wrt_en, reset, clk; 
-	input [3:0] func, op;
+	input [3:0] EX_func, EX_op;
 	input [DBITS-1:0] regData2, intermediateResult; 
 	input [REG_INDEX_BIT_WIDTH-1:0] rs2, rd; 
 	input ME_mux_sel, wrReg, wrMem;
 	
-	output [3:0] EX_func, EX_op;
-	output [DBITS-1:0] EX_regData2, EX_intermediateResult;
-	output [REG_INDEX_BIT_WIDTH-1:0] EX_rs2, EX_rd;
+	output[3:0] ME_func, ME_op;
+	output[DBITS-1:0] EX_regData2, EX_intermediateResult;
+	output[REG_INDEX_BIT_WIDTH-1:0] EX_rs2, EX_rd;
 	output EX_ME_mux_sel, EX_wrReg, EX_wrMem;
-		
-	Register #(4,0) funcReg(clk, reset, wrt_en, func, EX_func); 
-	Register #(4,0) opReg(clk, reset, wrt_en, op, EX_op);
+	
+	Register #(4,0) funcReg(clk, reset, wrt_en, func, ME_func); 
+	Register #(4,0) opReg(clk, reset, wrt_en, op, ME_op);
 	Register #(DBITS,0) regData2Reg(clk, reset, wrt_en, regData2, EX_regData2); 
 	Register #(DBITS,0) intermediateResultReg(clk, reset, wrt_en, intermediateResult, EX_intermediateResult);
 	Register #(REG_INDEX_BIT_WIDTH,0) rs2Reg(clk, reset, wrt_en, rs2, EX_rs2);
